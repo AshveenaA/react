56946751f19a125e2f8a486fe2526caf540d1ebc
Arunaraj <ashveena_arunaraj@optum.com>
2024-05-26 11:49:33 +0530

    first commit

---
+++ react-app/src/message.tsx
@@ -0,0 +1,11 @@
+function Message()
+{
+    //JSX:javascript xml
+    const name="sachin";
+   // return <h1>Hello </h1>;
+    if(name=="sachin")
+    {
+        return <h1>Good to see u {name}</h1>;
+    }
+}
+export default Message;
